47ff87b64447766d8f1a29db0291e66e18c52d48
Freddy-ux <alfredmuleri569@gmail.com>
2023-05-15 21:40:13 +0300

    Done with Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+  
+  greet("Naureen")
+
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+  greet_with_default("Naureen")
+
+  def add(a, b)
+    return a + b
+  end
+  
+  def halve(number)
+    return number / 2 if number.is_a?(Integer)
+    return nil
+  end
